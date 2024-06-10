640f343c5f28333815fd697647fce538e8ad4116
Brendan Stupik <106127309+BrendanStupik@users.noreply.github.com>
2024-06-10 17:48:48 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='SpotToBee',
-    version='1.0.4',
+    version='1.0.5',
     author='Brendan Stupik',
     author_email='spottobee@brendanstupik.anonaddy.com',
     description='Convert a CSV or Spotify playlist URL to a MusicBee smart playlist',
     long_description=long_description,
     long_description_content_type="text/markdown",
     license='MIT',
-    url='https://github.com/Brendan00x0/SpotToBee',
+    url='https://github.com/BrendanStupik/SpotToBee',
     packages=find_packages(),
     install_requires=[
         'spotipy>=2.0.0',
